56b563e2c97bd03dec9da0992a868bebbb0ab343
JohnAlexander Andrade Diaz <johnandrade18@hotmail.com>
2018-11-12 14:22:54 -0500

    pagina principal

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Bitacora de recaudo electronica">
+  <meta name="author" content="bre.com">
+  <meta name="keyword" content="Bitacora de recaudo electronica">
+
+  <title>BRE | Bitacora de recaudo electronica</title>
+  <link rel="icon" href="img/logop.png">
+
+  <!-- Icons -->
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/font-icoomons.css" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet">
+
+  <!--Boostrap-->
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+  <!-- Main styles for this application 
+  <link href="css/style.css" rel="stylesheet"> -->
+
+</head>
+
+<body class="app ">
+<nav class="navbar navbar-dark bg-transparent mx-auto text-black">
+  <a href="#" class="navbar-brand"><h1>Bitacora de recaudo electronica</h1></a>
+</nav>
+    <div class="container img-container mx-auto "></div>   
+<div class="container margen mx-auto mb-4">
+    <div class="well">
+        <h1 class="text-white">Inicio de sesión</h1>
+        <p class="text-muted">Control de acceso al sistema</p>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text icon-user-circle icofont" id="basic-addon1"></span>
+          </div>
+          <input type="email" id="correo" class="form-control form-control-lg" placeholder="Correo">
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text icon-locked icofont" id="basic-addon1"></span>
+          </div>
+          <input type="password" id="pass" class="form-control form-control-lg" placeholder="Contraseña">
+          <div class="input-group-append">
+            <button class="input-group-text" id="show-hide-passwd" action="hide"><span class="icon-eye1 icofont " ></span></button>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-12 px-0" >
+              <button type="submit" class="btn btn-primary " id="login">Entrar</button>
+              <button type="button" class="btn btn-link margen-left" ><span class="mx-auto text-right">Olvidaste tu password?</span> </button>
+        </div>
+</div>
+<footer class="footer mt-5">
+      <div class="">
+      <span><a href="http://www.coodetranspalmira.com ">Coodetrans Palmira</a> &copy; 2019</span>
+      <span class="mx-auto">Desarrollado por <a href="#">John Andrade</a></span>
+  </footer>
+
+  <!-- Bootstrap and necessary plugins -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
